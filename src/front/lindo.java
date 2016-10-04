b844a0a9b28da2b883f8df16c4487d1c2f6c66b2
Higor Rozan <higorb.rozan@gmail.com>
2016-10-03 22:46:43 -0300

    Add pacote front

--- src/front/lindo.java
+++ src/front/lindo.java
@@ -3,23 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cadastro;
-
-import model.*;
+package front;
 
 /**
  *
  * @author higor
  */
-public class ClienteCad {
-
-    private final Cliente c;
-    
-    public ClienteCad() {
-        this.c = new Cliente();
-        
-    }
-    
-    
+public class lindo {
     
 }
